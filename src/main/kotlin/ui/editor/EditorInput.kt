63c8fe5dd5418747ad60fabcedc4ef1bc441b770
Andrey Stepanov <stepanov.andre@gmail.com>
2023-12-20 16:32:04 +0200

    add selection implementation + copy and paste features

--- src/main/kotlin/ui/editor/EditorInput.kt
+++ src/main/kotlin/ui/editor/EditorInput.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.awtEventOrNull
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.input.pointer.pointerInput
+import java.awt.event.KeyEvent as AwtKeyEvent
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import util.lineCount
 import util.lineLength
 import kotlin.math.min
@@ -18,6 +22,7 @@ internal fun Modifier.pointerInput(editorState: EditorState): Modifier {
 
     return pointerInput(Unit) {
         detectTapGestures(onPress = { offset ->
+            editorState.clearSelection()
             cursorPosition.value = editorState.setCursorByCodePosition(offset)
             editorState.resetCursorBlinking()
         })
@@ -25,6 +30,7 @@ internal fun Modifier.pointerInput(editorState: EditorState): Modifier {
         detectDragGestures(
             onDragStart = { offset ->
                 oldDragOffset = offset
+                editorState.startSelection()
                 cursorPosition.value = editorState.setCursorByCodePosition(offset)
                 editorState.resetCursorBlinking()
             },
@@ -39,47 +45,108 @@ internal fun Modifier.pointerInput(editorState: EditorState): Modifier {
 
 private fun EditorState.setCursorByCodePosition(offset: Offset): CursorPosition {
     val (x, y) = viewportToCode(offset)
-    val newY = min(y, rope.lineCount - 1)
-    val lineLength = rope.lineLength(newY)
+    val newY = min(y, rope.value.lineCount - 1)
+    val lineLength = rope.value.lineLength(newY)
     val newX = min(x, lineLength)
     return CursorPosition(CodePosition(newX, newY), newX)
 }
 
-internal fun Modifier.keyboardInput(editorState: EditorState): Modifier =
+internal fun Modifier.keyboardInput(editorState: EditorState, clipboardManager: ClipboardManager): Modifier =
     onKeyEvent { keyEvent ->
-        val cursorPosition = editorState.cursorPosition
-        if (keyEvent.hasModifiers || keyEvent.type == KeyEventType.KeyUp) return@onKeyEvent false
+        keyEvent.awtEventOrNull?.let { awtEvent ->
+            if (awtEvent.id == AwtKeyEvent.KEY_TYPED) {
+                println("`${awtEvent.keyChar}` ${awtEvent.keyChar.code}")
+
+                val hasDeletedSelection = editorState.deleteSelection()
+
+                val rope = editorState.rope.value
+                val cursorPosition = editorState.cursorPosition.value
+
+                val ropeIndex = rope.indexOf(cursorPosition.codePosition)
+                val (newRope, newCursorPosition) = when (awtEvent.keyChar.code) {
+                    AwtKeyEvent.VK_ENTER -> {
+                        val newRope = rope.insert(ropeIndex, awtEvent.keyChar)
+                        val newCodePosition = cursorPosition.codePosition.let { it.copy(x = 0, y = it.y + 1) }
+                        newRope to CursorPosition(newCodePosition, newCodePosition.x)
+                    }
+
+                    AwtKeyEvent.VK_BACK_SPACE -> {
+                        if (hasDeletedSelection) {
+                            rope to cursorPosition
+                        } else if (ropeIndex != 0) {
+                            val newCodePosition = rope.codePositionOf(ropeIndex - 1)
+                            val newRope = rope.delete(ropeIndex - 1, ropeIndex)
+                            newRope to CursorPosition(newCodePosition, newCodePosition.x)
+                        } else {
+                            rope to cursorPosition.copy(wantedX = 0)
+                        }
+                    }
+
+                    AwtKeyEvent.VK_DELETE -> {
+                        if (hasDeletedSelection) {
+                            rope to cursorPosition
+                        } else if (ropeIndex != rope.length) {
+                            rope.delete(ropeIndex, ropeIndex + 1)
+                        } else {
+                            rope
+                        } to cursorPosition.copy(wantedX = cursorPosition.codePosition.x)
+                    }
+
+                    else -> {
+                        val newRope = rope.insert(
+                            editorState.rope.value.indexOf(cursorPosition.codePosition),
+                            awtEvent.keyChar
+                        )
+                        val newCodePosition = cursorPosition.codePosition.let { it.copy(x = it.x + 1) }
+                        newRope to CursorPosition(newCodePosition, newCodePosition.x)
+                    }
+                }
+                editorState.rope.value = newRope
+                editorState.cursorPosition.value = newCursorPosition
+            }
+        }
+
+        if (keyEvent.type != KeyEventType.KeyDown) return@onKeyEvent false
+
+        val rope = editorState.rope.value
+        val cursorPosition = editorState.cursorPosition.value
+
         when (keyEvent.key) {
             Key.DirectionRight -> {
-                cursorPosition.value = cursorPosition.value.let { (codePosition, _) ->
-                    val lineLength = editorState.rope.lineLength(codePosition.y)
-                    if (codePosition.x + 1 <= lineLength) {
-                        CursorPosition(codePosition.copy(x = codePosition.x + 1), codePosition.x + 1)
-                    } else if (codePosition.y + 1 < editorState.rope.lineCount) {
-                        CursorPosition(CodePosition(0, codePosition.y + 1), 0)
-                    } else {
-                        CursorPosition(codePosition, codePosition.x)
-                    }
+                if (keyEvent.isCtrlPressed || keyEvent.isAltPressed || keyEvent.isMetaPressed) return@onKeyEvent false
+
+                if (keyEvent.isShiftPressed) editorState.startOrContinueSelection()
+                else editorState.clearSelection()
+
+                editorState.cursorPosition.value = cursorPosition.let { (codePosition, _) ->
+                    val newIndex = rope.indexOf(codePosition) + 1
+                    val newCodePosition = rope.codePositionOf(newIndex, coerce = true)
+                    CursorPosition(newCodePosition, newCodePosition.x)
                 }
             }
 
             Key.DirectionLeft -> {
-                cursorPosition.value = cursorPosition.value.let { (codePosition, _) ->
-                    if (codePosition.x - 1 >= 0) {
-                        CursorPosition(codePosition.copy(x = codePosition.x - 1), codePosition.x - 1)
-                    } else if (codePosition.y - 1 >= 0) {
-                        val lineLength = editorState.rope.lineLength(codePosition.y - 1)
-                        CursorPosition(CodePosition(lineLength, codePosition.y - 1), lineLength)
-                    } else {
-                        CursorPosition(codePosition, codePosition.x)
-                    }
+                if (keyEvent.isCtrlPressed || keyEvent.isAltPressed || keyEvent.isMetaPressed) return@onKeyEvent false
+
+                if (keyEvent.isShiftPressed) editorState.startOrContinueSelection()
+                else editorState.clearSelection()
+
+                editorState.cursorPosition.value = cursorPosition.let { (codePosition, _) ->
+                    val newIndex = rope.indexOf(codePosition) - 1
+                    val newCodePosition = rope.codePositionOf(newIndex, coerce = true)
+                    CursorPosition(newCodePosition, newCodePosition.x)
                 }
             }
 
             Key.DirectionUp -> {
-                cursorPosition.value = cursorPosition.value.let { (codePosition, wantedX) ->
+                if (keyEvent.isCtrlPressed || keyEvent.isAltPressed || keyEvent.isMetaPressed) return@onKeyEvent false
+
+                if (keyEvent.isShiftPressed) editorState.startOrContinueSelection()
+                else editorState.clearSelection()
+
+                editorState.cursorPosition.value = cursorPosition.let { (codePosition, wantedX) ->
                     if (codePosition.y - 1 >= 0) {
-                        val lineLength = editorState.rope.lineLength(codePosition.y - 1)
+                        val lineLength = rope.lineLength(codePosition.y - 1)
                         val newCodePosition = CodePosition(min(wantedX, lineLength), codePosition.y - 1)
                         CursorPosition(newCodePosition, wantedX)
                     } else {
@@ -89,15 +156,37 @@ internal fun Modifier.keyboardInput(editorState: EditorState): Modifier =
             }
 
             Key.DirectionDown -> {
-                cursorPosition.value = cursorPosition.value.let { (codePosition, wantedX) ->
-                    if (codePosition.y + 1 < editorState.rope.lineCount) {
-                        val lineLength = editorState.rope.lineLength(codePosition.y + 1)
+                if (keyEvent.isCtrlPressed || keyEvent.isAltPressed || keyEvent.isMetaPressed) return@onKeyEvent false
+
+                if (keyEvent.isShiftPressed) editorState.startOrContinueSelection()
+                else editorState.clearSelection()
+
+                editorState.cursorPosition.value = cursorPosition.let { (codePosition, wantedX) ->
+                    if (codePosition.y + 1 < rope.lineCount) {
+                        val lineLength = rope.lineLength(codePosition.y + 1)
                         val newCodePosition = CodePosition(min(wantedX, lineLength), codePosition.y + 1)
                         CursorPosition(newCodePosition, wantedX)
                     } else {
-                        val lineLength = editorState.rope.lineLength(codePosition.y)
+                        val lineLength = rope.lineLength(codePosition.y)
                         CursorPosition(CodePosition(lineLength, codePosition.y), lineLength)
                     }
+                }
+            }
+
+            Key.C -> {
+                if ((keyEvent.isMetaPressed && !keyEvent.isCtrlPressed && !keyEvent.isAltPressed && !keyEvent.isShiftPressed)
+                    || (keyEvent.isCtrlPressed && !keyEvent.isCtrlPressed && !keyEvent.isAltPressed && !keyEvent.isShiftPressed)
+                ) {
+                    editorState.copySelection(clipboardManager)
+                }
+            }
+
+            Key.V -> {
+                if ((keyEvent.isMetaPressed && !keyEvent.isCtrlPressed && !keyEvent.isAltPressed && !keyEvent.isShiftPressed)
+                    || (keyEvent.isCtrlPressed && !keyEvent.isCtrlPressed && !keyEvent.isAltPressed && !keyEvent.isShiftPressed)
+                ) {
+                    editorState.deleteSelection()
+                    editorState.paste(clipboardManager)
                 }
             }
 
@@ -114,4 +203,52 @@ private val KeyEvent.hasModifiers
 
 private fun EditorState.resetCursorBlinking() {
     isCursorVisible.value = true to isCursorVisible.value.second + 1
+}
+
+private fun EditorState.startSelection() {
+    selectionStart.value = cursorPosition.value.codePosition
+}
+
+private fun EditorState.startOrContinueSelection() {
+    selectionStart.value = selectionStart.value ?: cursorPosition.value.codePosition
+}
+
+private fun EditorState.clearSelection() {
+    selectionStart.value = null
+}
+
+private fun EditorState.deleteSelection(): Boolean {
+    val (startPosition, endPosition) = getSelection() ?: return false
+
+    val currentRope = rope.value
+    val startIndex = currentRope.indexOf(startPosition)
+    val endIndex = currentRope.indexOf(endPosition)
+
+    selectionStart.value = startPosition
+    cursorPosition.value = CursorPosition(startPosition, startPosition.x)
+    rope.value = currentRope.delete(startIndex, endIndex)
+    return true
+}
+
+private fun EditorState.copySelection(clipboardManager: ClipboardManager) {
+    val (startPosition, endPosition) = getSelection() ?: return
+
+    val currentRope = rope.value
+    val startIndex = currentRope.indexOf(startPosition)
+    val endIndex = currentRope.indexOf(endPosition)
+
+    val text = currentRope.slice(startIndex, endIndex).toString()
+    clipboardManager.setText(AnnotatedString((text))) // TODO: copy with annotations when we have them
+}
+
+private fun EditorState.paste(clipboardManager: ClipboardManager) {
+    clipboardManager.getText()?.let { text ->
+        val currentRope = rope.value
+        val cursorIndex = currentRope.indexOf(cursorPosition.value.codePosition)
+        val newRope = currentRope.insert(cursorIndex, text.text)
+        val codePosition = newRope.codePositionOf(cursorIndex + text.text.length)
+
+        rope.value = newRope
+        cursorPosition.value = CursorPosition(codePosition, codePosition.x)
+    }
 }
--- src/main/kotlin/ui/editor/EditorViewModel.kt
+++ src/main/kotlin/ui/editor/EditorViewModel.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.v2.ScrollbarAdapter
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,9 +33,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import org.jetbrains.skia.Font
 import ui.styles.AppTheme
+import ui.styles.FontSettings
 import ui.styles.Settings
 import util.*
-import util.rope.Rope
+import util.rope.*
 import java.util.logging.Logger
 import kotlin.math.*
 
@@ -44,17 +46,17 @@ const val EDITOR_TEXT_OFFSET = GUTTER_SIZE + 2
 const val TOP_MARGIN = 0.5f
 
 object EditorView {
-    val log = Logger.getLogger(EditorView::class.java.name)
+    val log: Logger = Logger.getLogger(EditorView::class.java.name)
 }
 
 internal data class EditorState(
     val verticalScrollOffset: MutableState<Float>,
     val horizontalScrollOffset: MutableState<Float>,
     val canvasSize: MutableState<IntSize>,
-    val renderedText: MutableState<RenderedText?>,
     val cursorPosition: MutableState<CursorPosition>,
     val isCursorVisible: MutableState<Pair<Boolean, Int>>,
-    val rope: Rope<LineMetrics>,
+    val rope: MutableState<Rope<LineMetrics>>, // TODO: undo
+    val selectionStart: MutableState<CodePosition?>,
     val textSize: Size,
 )
 
@@ -67,21 +69,33 @@ fun BoxScope.EditorView(model: Editor, settings: Settings) = key(model) {
         verticalScrollOffset = remember { mutableStateOf(0f) },
         horizontalScrollOffset = remember { mutableStateOf(0f) },
         canvasSize = remember { mutableStateOf(IntSize.Zero) },
-        renderedText = remember { mutableStateOf(null) },
         cursorPosition = remember { mutableStateOf(CursorPosition(CodePosition(0, 0), 0)) },
         isCursorVisible = remember { mutableStateOf(true to 0) },
-        rope = model.rope,
-        textSize = remember(settings) { getTextSize(fontFamilyResolver, textMeasurer, settings) }
+        rope = remember { mutableStateOf(model.rope) },
+        selectionStart = remember { mutableStateOf(null) },
+        textSize = remember(settings.fontSettings) {
+            getTextSize(fontFamilyResolver, textMeasurer, settings.fontSettings)
+        }
     )
+
 
     val verticalScrollState = editorState.initVerticalScrollState()
     val horizontalScrollState = editorState.initHorizontalScrollState()
 
-    LaunchedEffect(editorState.verticalScrollOffset.value to settings.fontSize) {
-        editorState.rerenderText(settings, textMeasurer)
+    val renderedText = remember { mutableStateOf<RenderedText?>(null) }
+    val previousRope = remember { mutableStateOf<Rope<LineMetrics>?>(null) }
+    LaunchedEffect(
+        editorState.verticalScrollOffset.value,
+        editorState.canvasSize.value,
+        editorState.rope.value,
+        editorState.textSize,
+        settings.fontSettings
+    ) {
+        println("recompose")
+        editorState.rerenderText(renderedText, settings.fontSettings, textMeasurer, previousRope)
     }
 
-    LaunchedEffect(editorState.isCursorVisible.value) {
+    LaunchedEffect(editorState.isCursorVisible.value) { // TODO: documentation explicitly tells not to do that
         val time = editorState.isCursorVisible.value.second
         delay(500)
         editorState.isCursorVisible.value.takeIf { it.second == time }?.let {
@@ -97,7 +111,7 @@ fun BoxScope.EditorView(model: Editor, settings: Settings) = key(model) {
         .focusRequester(requester)
         .focusable()
         .onSizeChanged { editorState.canvasSize.value = it }
-        .keyboardInput(editorState)
+        .keyboardInput(editorState, LocalClipboardManager.current)
         .pointerInput(editorState)
         .scrollable(verticalScrollState, Orientation.Vertical)
         .scrollable(horizontalScrollState, Orientation.Horizontal)
@@ -107,7 +121,9 @@ fun BoxScope.EditorView(model: Editor, settings: Settings) = key(model) {
 
         drawRect(AppTheme.colors.material.background, size = this.size)
 
-        editorState.renderedText.value?.let {
+        drawSelection(editorState)
+
+        renderedText.value?.let {
             drawText(
                 it.textLayoutResult,
                 topLeft = editorState.codeToViewport(CodePosition(0, it.from))
@@ -123,17 +139,70 @@ fun BoxScope.EditorView(model: Editor, settings: Settings) = key(model) {
             )
         }
 
-        drawGutter(settings, textSize, verticalOffset, textMeasurer, editorState.rope.lineCount)
+        drawGutter(settings.fontSettings, textSize, verticalOffset, textMeasurer, editorState.rope.value.lineCount)
     }
 
     LaunchedEffect(Unit) {
-        requester.requestFocus() // TODO: focus system?
+        requester.requestFocus()
     }
     VerticalScrollbar(editorState)
     HorizontalScrollbar(editorState)
 }
 
-private fun getTextSize(fontFamilyResolver: FontFamily.Resolver, textMeasurer: TextMeasurer, settings: Settings): Size {
+
+private fun DrawScope.drawSelection(editorState: EditorState) {
+    val (startPosition, endPosition) = editorState.getSelection() ?: return
+    val textSize = editorState.textSize
+
+    if (startPosition.y == endPosition.y) {
+        drawRect(
+            topLeft = editorState.codeToViewport(startPosition),
+            size = Size((endPosition.x - startPosition.x) * textSize.width, textSize.height),
+            color = Color.Blue,
+            alpha = 0.3f
+        )
+    } else {
+        drawRect(
+            topLeft = editorState.codeToViewport(startPosition),
+            size = Size(size.width, textSize.height),
+            color = Color.Blue,
+            alpha = 0.3f
+        )
+        for (lineNumber in startPosition.y + 1 until endPosition.y) {
+            drawRect(
+                topLeft = editorState.codeToViewport(CodePosition(0, lineNumber)),
+                size = Size(size.width, textSize.height),
+                color = Color.Blue,
+                alpha = 0.3f
+            )
+
+        }
+        val (x, y) = editorState.codeToViewport(endPosition)
+        drawRect(
+            topLeft = Offset(0f, y),
+            size = Size(x, textSize.height),
+            color = Color.Blue,
+            alpha = 0.3f
+        )
+    }
+}
+
+internal fun EditorState.getSelection(): Pair<CodePosition, CodePosition>? {
+    val selectionStart = selectionStart.value
+    val cursorPosition = cursorPosition.value.codePosition
+
+    return if (selectionStart != null && selectionStart != cursorPosition) {
+        val startPosition = minOf(selectionStart, cursorPosition, compareBy({ it.y }, { it.x }))
+        val endPosition = if (startPosition != selectionStart) selectionStart else cursorPosition
+        startPosition to endPosition
+    } else null
+}
+
+private fun getTextSize(
+    fontFamilyResolver: FontFamily.Resolver,
+    textMeasurer: TextMeasurer,
+    settings: FontSettings
+): Size {
     val fontLoadResult = fontFamilyResolver.resolve(settings.fontFamily).value as FontLoadResult
     val typeface = fontLoadResult.typeface
     val width = Font(typeface, settings.fontSize.value).measureTextWidth("x") * 2
@@ -154,6 +223,19 @@ internal fun EditorState.viewportToCode(offset: Offset): CodePosition = CodePosi
 internal data class CodePosition(val x: Int, val y: Int)
 internal data class CursorPosition(val codePosition: CodePosition, val wantedX: Int)
 
+internal fun Rope<LineMetrics>.indexOf(codePosition: CodePosition): Int =
+    getIndexOfKthLine(codePosition.y) + codePosition.x
+
+internal fun Rope<LineMetrics>.codePositionOf(index: Int, coerce: Boolean = false): CodePosition {
+    require(coerce || index in 0..length) { "Index out of bounds!" }
+    val coercedIndex = index.coerceIn(0..length)
+
+    val slice = slice(0, coercedIndex)
+    val y = slice.lineCount - 1
+    val x = coercedIndex - getIndexOfKthLine(y)
+    return CodePosition(x, y)
+}
+
 @Composable
 private fun BoxScope.HorizontalScrollbar(editorState: EditorState) {
     HorizontalScrollbar(
@@ -171,7 +253,7 @@ private fun BoxScope.HorizontalScrollbar(editorState: EditorState) {
             }
 
         },
-        // idk why / 2, it just doesn't work without it
+
         Modifier.align(Alignment.BottomCenter).absolutePadding(left = (GUTTER_SIZE * editorState.textSize.width / 2).dp)
     )
 }
@@ -214,26 +296,29 @@ private fun EditorState.initVerticalScrollState() = rememberScrollableState { de
 }
 
 private suspend fun EditorState.rerenderText(
-    settings: Settings,
-    textMeasurer: TextMeasurer
+    renderedText: MutableState<RenderedText?>,
+    settings: FontSettings,
+    textMeasurer: TextMeasurer,
+    previousRope: MutableState<Rope<LineMetrics>?>
 ): Unit = withContext(Dispatchers.Default) {
     val text = renderedText.value
     val verticalOffset = verticalScrollOffset.value
     val canvasSize = canvasSize.value
 
-    if (text == null
+    if (text == null || previousRope.value != rope.value
         || text.textSize != settings.fontSize
         || (text.from > 0
                 && verticalOffset - text.from * textSize.height < canvasSize.height / 2)
-        || (text.to < rope.lineCount
+        || (text.to < rope.value.lineCount
                 && text.to * textSize.height - verticalOffset - canvasSize.height < canvasSize.height / 2)
     ) {
+        previousRope.value = rope.value
         val from = floor((verticalOffset - canvasSize.height) / textSize.height).toInt()
             .coerceAtLeast(0)
         val to = ceil((verticalOffset + 2 * canvasSize.height) / textSize.height).toInt()
-            .coerceAtMost(rope.lineCount)
+            .coerceAtMost(rope.value.lineCount)
         EditorView.log.info("Relayout from $from to $to")
-        renderedText.value = textMeasurer.layoutLines(rope, from, to, settings)
+        renderedText.value = textMeasurer.layoutLines(rope.value, from, to, settings)
         // in case text size has changed we want to maintain correct verticalScrollOffset
         verticalScrollOffset.value = coerceVerticalOffset(verticalScrollOffset.value)
     }
@@ -248,13 +333,13 @@ private fun EditorState.coerceHorizontalOffset(offset: Float) = offset
     .coerceAtMost(getMaxHorizontalScroll())
 
 private fun EditorState.getMaxVerticalScroll() =
-    ((rope.lineCount + 5) * textSize.height - canvasSize.value.height).coerceAtLeast(0f)
+    ((rope.value.lineCount + 5) * textSize.height - canvasSize.value.height).coerceAtLeast(0f)
 
 private fun EditorState.getMaxHorizontalScroll() =
-    ((rope.maxLineLength + GUTTER_SIZE) * textSize.width - canvasSize.value.width).coerceAtLeast(0f)
+    ((rope.value.maxLineLength + GUTTER_SIZE) * textSize.width - canvasSize.value.width).coerceAtLeast(0f)
 
 private fun DrawScope.drawGutter(
-    settings: Settings,
+    settings: FontSettings,
     textSize: Size,
     verticalScrollOffset: Float,
     textMeasurer: TextMeasurer,
@@ -297,7 +382,7 @@ private fun TextMeasurer.layoutLines(
     rope: Rope<LineMetrics>,
     from: Int,
     to: Int,
-    settings: Settings
+    settings: FontSettings
 ): RenderedText {
     val builder = AnnotatedString.Builder()
     val text = rope.getLines(from, to)
@@ -312,5 +397,5 @@ private fun TextMeasurer.layoutLines(
     return RenderedText(textLayoutResult, from, to, settings.fontSize)
 }
 
-private fun getTextStyle(settings: Settings) =
+private fun getTextStyle(settings: FontSettings) =
     TextStyle(fontFamily = settings.fontFamily, fontSize = settings.fontSize)
