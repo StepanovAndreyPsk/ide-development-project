2c569f57a8115d618ce4d87a546f6bff9def6a68
Andrey Stepanov <stepanov.andre@gmail.com>
2024-01-20 14:29:34 +0200

    parser

---
+++ src/main/kotlin/idelang/parser/parser.kt
@@ -0,0 +1,32 @@
+package idelang.parser
+
+import idelang.ast.Program
+import idelang.grammar.AProgram
+import idelang.lexer.AScriptLexer
+import language.structures.ASTBuilder
+
+
+fun createProgram(input: CharSequence) : Result<Program> {
+    val tokens = AScriptLexer().tokenize(input)
+    println(tokens)
+    return ASTBuilder(AProgram(), tokens).build()
+}
+
+
+fun main() {
+    val myProgramm =
+        """  
+        func add(a: number, b: number)
+        {
+           return a + b;
+        }
+        func main() {
+        }
+        """
+
+    createProgram(myProgramm).onSuccess {
+        println(it)
+    }.onFailure {
+        println(it)
+    }
+}
